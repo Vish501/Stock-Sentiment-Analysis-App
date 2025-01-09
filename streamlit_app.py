b94fa803b3f5dd6222663cb721ef22c59ac50ee5
Vishal <76504332+Vish501@users.noreply.github.com>
2025-01-09 14:28:04 +0000

    updated

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,23 @@
 import streamlit as st
+from transformers import pipeline
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+# Creating the sentiment analysis pipeline
+classifier = pipeline(task='text-classification', model='ProsusAI/finbert')
+
+
+def feed(ticker):
+    rss_url = f'https://feeds.finance.yahoo.com/rss/2.0/headline?s={ticker}&lang=en-US'
+    return feedparser.parse(rss_url)
+
+
+def main():
+    st.title('#📈 TICKER SENTIMENET ANALYZER')
+    st.write('Enter a ticker and get the news sentiment for it.')
+
+    if prompt := st.chat_input("Ticker"):
+        pass
+
+
+
+if __name__ == '__main__':
+    main()
